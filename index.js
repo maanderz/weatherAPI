c3cbda85c8c0e2ad740072cb48b679e2a032c521
maanderz <mandyfung93@gmail.com>
2021-01-25 16:17:11 -0700

    remove console logs

--- index.js
+++ index.js
@@ -9,31 +9,19 @@ const port = 3001
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => {
-    res.send('HELLO!')
-})
-
 app.get('/', (req, res) => {
-    // console.log('123', req.query)
     try {        
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${process.env.API_KEY}&units=metric`)
             .then((response) => {
-                console.log('response', response.data)
-
-                let modifiedData = {
-                    status: response.data.weather[0].main,
-                    description: response.data.weather[0].description,
-                    temperature: response.data.main.temp,
-                    wind: response.data.wind.speed,
-                    humidity: response.data.main.humidity
-                };
-                res.json(modifiedData)
-
-                
-                // res.json(response.data)
+            let modifiedData = {
+                status: response.data.weather[0].main,
+                description: response.data.weather[0].description,
+                temperature: response.data.main.temp,
+                wind: response.data.wind.speed,
+                humidity: response.data.main.humidity
+            };
+            res.json(modifiedData)
         })
-
-        // res.send('Hello world')
     } catch (err) {
         res.send({err: err});
     }
